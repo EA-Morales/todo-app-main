e56aa4b53f9c5f12e51d9677e26ee5e94715c5f2
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-10-15 22:34:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App(): JSX.Element {
+	return (
+		<div>
+			<h1 className='text-9xl text-red-600'>HOLA MUNDO</h1>
+			<div className='attribution'>
+				Challenge by{' '}
+				<a
+					href='https://www.frontendmentor.io?ref=challenge'
+					rel='noreferrer'
+					target='_blank'>
+					Frontend Mentor
+				</a>
+				. Coded by <a href='#'>Your Name Here</a>.
+			</div>
+		</div>
+	);
+}
+
+export default App;
