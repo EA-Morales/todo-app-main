ed2df2c6493b1e907f2f5033e7594dc9b63234d4
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-10-19 22:16:46 -0300

    styles changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,85 @@
-import React, { FormEvent, useState } from "react";
-
-interface todos {
-	name: string;
-	completed: boolean;
-}
-
-const list: todos[] = [
-	{
-		name: "todos 1",
-		completed: true,
-	},
-	{
-		name: "todos 2",
-		completed: false,
-	},
-];
+import background from "./assets/images/bg-mobile-light.jpg";
+import Moon from "./assets/icons/Moon";
 
 function App(): JSX.Element {
-	const [todos, setTodos] = useState<todos[]>(list);
-	const [completed, setCompleted] = useState(false);
-	const [title, setTitle] = useState("");
+  return (
+    <div
+      className="h-screen bg-slate-200 bg-no-repeat p-4"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}>
+      <header className="container mt-[50px] w-full">
+        <section className="flex justify-between">
+          <h1 className="text-xl font-bold tracking-[0.5rem] text-white">
+            TODO
+          </h1>
+          <button>
+            <Moon className="h-5 w-5 text-white" />
+          </button>
+        </section>
 
-	function handleSubmit(e: FormEvent<HTMLFormElement>): void {
-		e.preventDefault();
-		console.log(title);
-		setTitle("");
-	}
+        <form className="mt-10 flex w-full items-center gap-2 rounded-lg bg-white py-2 pl-5 pr-2">
+          <button
+            className="h-5 w-5 rounded-full border-[1px]"
+            type="button"
+          />
+          <input
+            className="w-full"
+            placeholder="Create a new todo..."
+            type="text"
+          />
+        </form>
+      </header>
 
-	return (
-		<div className="container">
-			<header className="h-32 bg-[url('../src/assets/images/bg-mobile-light.jpg')] bg-cover bg-no-repeat">
-				<div className="flex flex-col px-4 pt-6">
-					<div className="flex justify-between">
-						<h1 className="text-2xl font-bold tracking-[0.5em] text-white">
-							TODO
-						</h1>
-						<span>lunita</span>
-					</div>
-					<div className="flex items-center gap-4 rounded-lg bg-white p-2">
-						<button
-							className={
-								completed
-									? `h-5 w-5 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500`
-									: `h-5 w-5 rounded-full bg-slate-500`
-							}
-							onClick={() => setCompleted(!completed)}
-						/>
-						<form
-							className=""
-							onSubmit={handleSubmit}>
-							<input
-								className="w-full"
-								id="title"
-								name="title"
-								placeholder="Create a new task"
-								type="text"
-								value={title}
-								onChange={(e) => setTitle(e.target.value)}
-							/>
-						</form>
-					</div>
-				</div>
-			</header>
-			{todos.map((todo) => (
-				<li key={todo.name}>{todo.name}</li>
-			))}
-			<div className="attribution">
-				Challenge by{" "}
-				<a
-					href="https://www.frontendmentor.io?ref=challenge"
-					rel="noreferrer"
-					target="_blank">
-					Frontend Mentor
-				</a>
-				. Coded by <a href="#">Your Name Here</a>.
-			</div>
-		</div>
-	);
+      <main className="mt-4">
+        <article className="flex gap-2 rounded-lg border-b-[1px] border-slate-400 bg-white py-2 px-5">
+          <button className="h-5 w-5 rounded-full border-[1px]" />
+          <p className="grow">Terminar esta mierda</p>
+          <span>X</span>
+        </article>
+        <article className="flex gap-2 rounded-lg border-b-[1px] border-slate-400 bg-white py-2 px-5">
+          <button className="h-5 w-5 rounded-full border-[1px]" />
+          <p className="grow">Terminar esta mierda</p>
+          <span>X</span>
+        </article>
+        <article className="flex gap-2 rounded-lg border-b-[1px] border-slate-400 bg-white py-2 px-5">
+          <button className="h-5 w-5 rounded-full border-[1px]" />
+          <p className="grow">Terminar esta mierda</p>
+          <span>X</span>
+        </article>
+        <article className="flex gap-2 rounded-lg border-b-[1px] border-slate-400 bg-white py-2 px-5">
+          <button className="h-5 w-5 rounded-full border-[1px]" />
+          <p className="grow">Terminar esta mierda</p>
+          <span>X</span>
+        </article>
+        <section className="flex justify-between gap-2 rounded-lg border-b-[1px] border-slate-400 bg-white px-5 py-2 text-slate-400">
+          <p>5 items left</p>
+          <button>Clear Completed</button>
+        </section>
+
+        <section className="mt-4 flex justify-center gap-4 rounded-lg border-b-[1px] border-slate-400 bg-white px-5 py-2 text-slate-400">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </section>
+
+        <p className="mt-4 text-center text-slate-400">
+          Drag and drop to reorder list
+        </p>
+      </main>
+
+      <footer className="absolute bottom-0">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          rel="noreferrer"
+          target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
